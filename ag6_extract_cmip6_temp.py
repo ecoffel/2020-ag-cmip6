a77cdb2b9d6143c42ecb30e4ec7ffc5859e2bae0
edcoffel <edcoffel@noreply.syr.edu>
2021-04-09 15:40:33 -0400

    ag6 su

--- ag6_extract_cmip6_temp.py
+++ ag6_extract_cmip6_temp.py
@@ -17,9 +17,11 @@ dirDeepak = '/home/edcoffel/drive/MAX-Filer/Research/Climate-01/Personal-F20/edc
 # cmip6_models = ['access-cm2', 'access-esm1-5', 'awi-cm-1-1-mr', 'bcc-csm2-mr', 'bcc-esm1', 'canesm5', 'ec-earth3', \
 #                 'gfdl-cm4', 'gfdl-esm4', 'giss-e2-1-g', 'kace-1-0-g', 'fgoals-g3', 'inm-cm5-0', 'ipsl-cm6a-lr', 'miroc6', \
 #                 'mpi-esm1-2-hr', 'mpi-esm1-2-lr', 'mri-esm2-0', 'noresm2-lm', 'noresm2-mm', 'sam0-unicon']
-cmip6_models = ['access-cm2', 'access-esm1-5', 'awi-cm-1-1-mr', 'bcc-esm1', 'canesm5', 'ec-earth3', \
-                'giss-e2-1-g', 'kace-1-0-g', 'fgoals-g3', 'inm-cm5-0', 'ipsl-cm6a-lr', 'miroc6', \
-                'mpi-esm1-2-hr', 'mpi-esm1-2-lr', 'mri-esm2-0', 'noresm2-lm', 'noresm2-mm', 'sam0-unicon']
+
+cmip6_models = ['access-cm2', 'access-esm1-5', 'awi-cm-1-1-mr', 'awi-esm-1-1-lr', 'bcc-esm1', 'canesm5', 'cmcc-esm2',
+                 'ec-earth3', 'fgoals-f3-l', 'fgoals-g3', 'giss-e2-1-g', 'inm-cm4-8', 'inm-cm5-0', 'ipsl-cm6a-lr',
+                 'ipsl-cm6a-lr-inca', 'kiost-esm', 'miroc6', 'mpi-esm1-2-ham', 'mpi-esm1-2-hr', 'mpi-esm1-2-lr',
+                 'mri-esm2-0', 'noresm2-lm', 'noresm2-mm']
 
 region = 'global'
 var = 'tasmin'
@@ -31,30 +33,33 @@ elif region == 'us':
     latRange = [20, 55]
     lonRange = [220, 300]
 
+    
+def in_time_range(y):
+    return (y >= 1981) & (y <= 2014)
 
 n = 0
 for i, model in enumerate(cmip6_models):
     
-    if os.path.isfile('cmip6_output/cmip6_%s_max_%s_%s.nc'%(var, region, model)):
-        continue
+#     if os.path.isfile('cmip6_output/cmip6_%s_max_%s_%s.nc'%(var, region, model)):
+#         continue
     
     print('opening %s...'%model)
     cmip6_temp_hist = xr.open_mfdataset('%s/%s/r1i1p1f1/historical/%s/*.nc'%(dirCmip6, model, var), concat_dim='time')
     
     print('selecting data for %s...'%model)
     cmip6_temp_hist = cmip6_temp_hist.sel(lat=slice(latRange[0], latRange[1]), \
-                                             lon=slice(lonRange[0], lonRange[1]), \
-                                            time=slice('1981-01', '2014-12'))
+                                             lon=slice(lonRange[0], lonRange[1]))
+    cmip6_temp_hist = cmip6_temp_hist.sel(time=in_time_range(cmip6_temp_hist['time.year']))
     
     print('resampling %s...'%model)
-    cmip6_max_hist = cmip6_temp_hist.resample(time='1Y').max()
-    cmip6_median_hist = cmip6_temp_hist.resample(time='1Y').median()
+    cmip6_max_hist = cmip6_temp_hist.resample(time='1Y').max(dim='time')
+    cmip6_mean_hist = cmip6_temp_hist.resample(time='1Y').mean(dim='time')
     
     cmip6_max_hist[var] -= 273.15
-    cmip6_median_hist[var] -= 273.15
+    cmip6_mean_hist[var] -= 273.15
     
     cmip6_max_ds = xr.Dataset()
-    cmip6_median_ds = xr.Dataset()
+    cmip6_mean_ds = xr.Dataset()
     
     if n == 0:
         timeVar = cmip6_max_hist.time
@@ -64,19 +69,19 @@ for i, model in enumerate(cmip6_models):
                           coords = {'time': timeVar, 'lat':cmip6_max_hist.lat, 'lon':cmip6_max_hist.lon},
                           attrs  = {'units'     : 'C'
                             })
-    cmip6_max_ds['temp_max'] = tempDs_max
+    cmip6_max_ds['%s_max'%var] = tempDs_max
     
-    tempDs_median = xr.DataArray(data   = cmip6_median_hist[var], 
+    tempDs_mean = xr.DataArray(data   = cmip6_mean_hist[var], 
                           dims   = ['time', 'lat', 'lon'],
-                          coords = {'time': timeVar, 'lat':cmip6_median_hist.lat, 'lon':cmip6_median_hist.lon},
+                          coords = {'time': timeVar, 'lat':cmip6_mean_hist.lat, 'lon':cmip6_mean_hist.lon},
                           attrs  = {'units'     : 'C'
                             })
-    cmip6_median_ds['temp_median'] = tempDs_median
+    cmip6_mean_ds['%s_mean'%var] = tempDs_mean
     
     
     print('saving netcdf...')
-    cmip6_max_ds.to_netcdf('cmip6_output/cmip6_%s_max_%s_%s1.nc'%(var, region, model))
-    cmip6_median_ds.to_netcdf('cmip6_output/cmip6_%s_median_%s_%s1.nc'%(var, region, model))
+    cmip6_max_ds.to_netcdf('cmip6_output/cmip6_%s_max_%s_%s9.nc'%(var, region, model))
+    cmip6_mean_ds.to_netcdf('cmip6_output/cmip6_%s_mean_%s_%s9.nc'%(var, region, model))
     
     print()
     n += 1
--- ag6_extract_era5_temp.py
+++ ag6_extract_era5_temp.py
@@ -12,8 +12,8 @@ import datetime
 
 dirERA5 = '/home/edcoffel/drive/MAX-Filer/Research/Climate-02/Data-02-edcoffel-F20/ERA5'
 
-file_var = 'tasmax'
-orig_var = 'mx2t'
+file_var = 'tasmin'
+orig_var = 'mn2t'
 
 latRange = [-90, 90]
 lonRange = [0, 360]
@@ -25,17 +25,17 @@ print('resampling era5...')
 temp_max_era5 = temp_era5.resample(time='1Y').max(dim='time')
 temp_max_era5[orig_var] -= 273.15
 
-temp_median_era5 = temp_era5.resample(time='1Y').median(dim='time')
-temp_median_era5[orig_var] -= 273.15
+temp_mean_era5 = temp_era5.resample(time='1Y').mean(dim='time')
+temp_mean_era5[orig_var] -= 273.15
 
 print('renaming dims...')
 temp_max_era5 = temp_max_era5.rename_dims(latitude='lat', longitude='lon')
-temp_median_era5 = temp_median_era5.rename_dims(latitude='lat', longitude='lon')
+temp_mean_era5 = temp_mean_era5.rename_dims(latitude='lat', longitude='lon')
 
 temp_max_era5 = temp_max_era5.rename({'latitude':'lat', 'longitude':'lon', orig_var:'%s_max'%file_var})
-temp_median_era5 = temp_median_era5.rename({'latitude':'lat', 'longitude':'lon', orig_var:'%s_median'%file_var})
+temp_mean_era5 = temp_mean_era5.rename({'latitude':'lat', 'longitude':'lon', orig_var:'%s_mean'%file_var})
 
-print('writing era5 txx...')
-temp_max_era5.to_netcdf('era5_%s_max_global.nc'%file_var)
-print('writing era5 t50p...')
-temp_median_era5.to_netcdf('era5_%s_median_global.nc'%file_var)
+# print('writing era5 txx...')
+temp_max_era5.to_netcdf('era5_%s_max_global3.nc'%file_var)
+print('writing era5 mean...')
+temp_mean_era5.to_netcdf('era5_%s_mean_global3.nc'%file_var)
